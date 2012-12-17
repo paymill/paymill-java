fff94046af7575126fc5bc2716307472a90baaa5
namero999 <co.alesso@fastwebnet.it>
2012-12-17 13:19:26 +0100

    Added PaymentService.create(token, client) and test case for PaymentService.delete

--- src/main/java/de/paymill/service/PaymentService.java
+++ src/main/java/de/paymill/service/PaymentService.java
@@ -6,6 +6,7 @@ package de.paymill.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.paymill.model.Client;
 import de.paymill.model.Payment;
 import de.paymill.net.HttpClient;
 
@@ -28,5 +29,15 @@ public class PaymentService extends AbstractService<Payment> {
 		params.put("token", token);
 		return client.post(resource, params, modelClass);
 	}
+
+	// Documented here, but missing
+	// https://www.paymill.com/it-it/documentation-3/reference/api-reference/index.html#create-new-credit-card-payment-with
+	// Not having this method, will cause problem during subscription management (eg, duplicate clients).
+    public Payment create(String token, Client client) {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("token", token);
+        params.put("client", client.getId());
+        return this.client.post(resource, params, modelClass);
+    }
 
 }
