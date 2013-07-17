b4550653164b401a697ff14770c39c45bf158cc8
Andreas Lüdeke <aluedeke@testobject.org>
2013-07-17 10:39:48 +0200

    update cancelAtPeriodEnd for subscription doesn't work
    update cancelAtPeriodEnd doesn't work as well

--- src/main/java/de/paymill/service/SubscriptionService.java
+++ src/main/java/de/paymill/service/SubscriptionService.java
@@ -19,7 +19,7 @@ public class SubscriptionService extends AbstractService<Subscription> {
 	public SubscriptionService(HttpClient client) {
 		super("Subscriptions", Subscription.class, client);
 		this.updateableProperties.add("offer");
-		this.updateableProperties.add("cancel_at_period_end");
+		this.updateableProperties.add("cancelAtPeriodEnd");
 	}
 
 	/**
